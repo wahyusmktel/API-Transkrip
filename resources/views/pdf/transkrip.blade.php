d005007acfb7b1d933216d3e3b576c30447f2b4b
Wahyu Rahmat Hidayat <wahyu@smktelkom-lpg.sch.id>
2025-06-19 16:58:19 +0700

    ok

--- resources/views/pdf/transkrip.blade.php
+++ resources/views/pdf/transkrip.blade.php
@@ -41,7 +41,7 @@
             font-size: 16px;
             font-weight: bold;
             margin-top: -10px;
-            margin-bottom: 5px;
+            margin-bottom: -3px;
         }
 
         .no-transkrip {
@@ -129,7 +129,7 @@
         transform: translate(-50%, -50%);
         opacity: 0.05;
         z-index: 0;
-        width: 60%;
+        width: 90%;
         text-align: center;
     ">
             <img src="{{ $watermarkBase64 }}" alt="Watermark" style="width: 100%; height: auto;" />
@@ -156,11 +156,11 @@
         {{-- Biodata Siswa --}}
         <div class="biodata">
             <div><span class="label">Satuan Pendidikan</span>: {{ $schoolConfig->nama_sekolah ?? '-' }}</div>
-            <div><span class="label">NPSN</span>: {{ $schoolConfig->npsn ?? '-' }}</div>
+            <div><span class="label">Nomor Pokok Sekolah Nasional</span>: {{ $schoolConfig->npsn ?? '-' }}</div>
             <div><span class="label">Nama Lengkap</span>: {{ $siswa->nama_lengkap }}</div>
-            <div><span class="label">Tempat, Tanggal Lahir</span>: {{ $siswa->tempat_lahir }},
+            <div><span class="label">Tempat dan Tanggal Lahir</span>: {{ $siswa->tempat_lahir }},
                 {{ \Carbon\Carbon::parse($siswa->tanggal_lahir)->translatedFormat('d F Y') }}</div>
-            <div><span class="label">NISN</span>: {{ $siswa->nisn }}</div>
+            <div><span class="label">Nomor Induk Siswa Nasional</span>: {{ $siswa->nisn }}</div>
             <div><span class="label">Nomor Ijazah</span>: {{ $siswa->nomor_ijazah }}</div>
             <div><span class="label">Tanggal Kelulusan</span>:
                 {{ \Carbon\Carbon::parse($transcriptConfig->tanggal_kelulusan)->translatedFormat('d F Y') }}</div>
@@ -172,9 +172,9 @@
         <table>
             <thead>
                 <tr>
-                    <th style="width: 5%; text-align:center;">No</th>
-                    <th style="width: 50%; text-align:center;">Mata Pelajaran</th>
-                    <th style="width: 20%; text-align:center;">Nilai</th>
+                    <th style="width: 5%; text-align:center; padding: 10px 0;">No</th>
+                    <th style="width: 50%; text-align:center; padding: 10px 0;">Mata Pelajaran</th>
+                    <th style="width: 20%; text-align:center; padding: 10px 0;">Nilai</th>
                 </tr>
             </thead>
             <tbody>
@@ -202,7 +202,7 @@
                                 <tr>
                                     <td rowspan="3" style="text-align:center; vertical-align: top;">
                                         {{ $no++ }}</td>
-                                    <td colspan="2"><strong>Muatan Lokal</strong></td>
+                                    <td colspan="2" style="padding-left: 5px;"><strong>Muatan Lokal</strong></td>
                                 </tr>
                                 @php
                                     $showMulokHeader = true;
@@ -212,13 +212,14 @@
 
                             <tr>
 
-                                <td>{{ $abjad++ }}. {{ $item->mapel->nama_mata_pelajaran }}</td>
+                                <td style="padding-left: 5px;">{{ $abjad++ }}.
+                                    {{ $item->mapel->nama_mata_pelajaran }}</td>
                                 <td style="text-align:center;">{{ $item->nilai }}</td>
                             </tr>
                         @else
                             <tr>
                                 <td style="text-align:center;">{{ $no++ }}</td>
-                                <td>{{ $item->mapel->nama_mata_pelajaran }}</td>
+                                <td style="padding-left: 5px;">{{ $item->mapel->nama_mata_pelajaran }}</td>
                                 <td style="text-align:center;">{{ $item->nilai }}</td>
                             </tr>
                         @endif
@@ -239,8 +240,9 @@
         <div class="ttd">
             <p>{{ $schoolConfig->kota ?? '-' }},
                 {{ \Carbon\Carbon::parse($transcriptConfig->tanggal_transkrip)->translatedFormat('d F Y') }}</p>
-            <p style="margin-bottom: 60px;">Kepala Sekolah</p>
-            <p style="font-weight: bold;">{{ $schoolConfig->nama_kepala_sekolah ?? '-' }}</p>
+            <p style="margin-bottom: 70px;">Kepala Sekolah</p>
+            <p style="font-weight: bold; text-decoration: underline;">{{ $schoolConfig->nama_kepala_sekolah ?? '-' }}
+            </p>
             <p>NIP. {{ $schoolConfig->nip_kepala_sekolah ?? '-' }}</p>
         </div>
     </div>
